43f7a4992dd9fabf3d2e6f222aef60339338910f
Keyur Bele <keyur.bele04@gmail.com>
2026-05-11 18:41:58 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,25 @@
 import { authMiddleware } from "@clerk/nextjs";
 
+
+
 export default authMiddleware({
-  // Allows users to see the landing/orb before logging in
+
+  // Routes that can be accessed while signed out
+
   publicRoutes: ["/"],
+
+  // Routes that can always be accessed, and have no authentication information
+
+  ignoredRoutes: ["/no-auth-in-here"],
+
 });
 
+
+
 export const config = {
+
+  // Protects all routes, including api/trpc
+
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+
 };
